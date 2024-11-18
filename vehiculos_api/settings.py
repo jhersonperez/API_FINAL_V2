97e9fed4c08f0232b88cc60e5d2c3236b6a32e71
Jherson Haruin Perez <jherson.perez@asmetsaludeps.local>
2024-11-18 15:13:29 -0500

    sexto commit

--- vehiculos_api/settings.py
+++ vehiculos_api/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
 }
 
 database_url = os.environ.get('DATABASE_URL')
-
+DATABASES['default'] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
