7384ea27335b6689b3194ad23cd61993d6bd6cbd
Jherson Haruin Perez <jherson.perez@asmetsaludeps.local>
2024-11-18 13:13:29 -0500

    segundo commit

--- vehiculos_api/settings.py
+++ vehiculos_api/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.enviroment.get('SECRET_KEY', default='asasasasasas')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='asasasasasas')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
